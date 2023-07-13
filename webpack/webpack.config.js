6ea095fbe35300221f07e0268f16ea8b616f18bb
Vladyslav Fedorov <vladyslav.fedorov@honeycombsoft.com>
2023-07-13 14:43:43 +0300

    Styles, images and fonts rules

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,32 +1,40 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: path.resolve(__dirname, '..', './src/index.tsx'),
     resolve: {
-        extensions: ['.tsx', '.ts', '.js']
+        extensions: ['.tsx', '.ts', '.js'],
     },
     module: {
         rules: [
             {
                 test: /\.(ts|js)x?$/,
                 exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                    }
-                ]
+                use: ['babel-loader'],
             },
-        ]
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
+                type: 'asset/resource',
+            },
+        ],
     },
     output: {
         path: path.resolve(__dirname, '..', './build'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
     },
     mode: 'development',
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '..', './src/index.html')
+            template: path.resolve(__dirname, '..', './src/index.html'),
         }),
-    ]
-}
+    ],
+};
